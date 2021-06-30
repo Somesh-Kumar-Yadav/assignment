663d6ac1894652bb92357dd4475429025decc6fb
Somesh <somesh_nj2_076@masai.school>
2021-06-30 20:38:30 +0530

    FOOD APP I

---
+++ unit-3-30-jun/FOOD APP I/components/navbar.js
@@ -0,0 +1,10 @@
+let navbar = () => {
+	return `<a href="#"><p class="logo">MY FOOD APP</p></a>
+				<ul>
+					<li><a href="search.html">Search</a></li>
+					<li><a href="receipe.html">Today's Receipe</a></li>
+					<li><a href="latest.html">Latest Receipe</a></li>
+					<li><a href="#">Login</a></li>
+				</ul>`;
+};
+export default navbar;
