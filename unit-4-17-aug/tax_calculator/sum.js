d25db35d3b6260b91d2a5d7f38f4d7002282c096
Somesh <somesh_nj2_076@masai.school>
2021-08-17 20:09:04 +0530

    tax_calculator assignment

---
+++ unit-4-17-aug/tax_calculator/sum.js
@@ -0,0 +1,11 @@
+function sum(a, b) {
+	if (arguments.length !== 2) {
+		return "invalid no of arguments";
+	}
+	if (typeof a !== "number" || typeof b !== "number") {
+		return "invalid";
+	}
+	return a + b;
+}
+
+module.exports = sum;
