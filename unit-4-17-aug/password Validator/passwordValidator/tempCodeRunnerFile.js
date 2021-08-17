0260870e30c0fd43c502ff94d70f185596edd7e7
Somesh <somesh_nj2_076@masai.school>
2021-08-17 22:01:50 +0530

    password assignment

---
+++ unit-4-17-aug/password Validator/passwordValidator/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+expect("sdsaiukjhldJ").validate(false);
+	// expect("jhldJ").validate(false);
+	// expect("5#kdsJ").validate(true);
+	// expect("5iukjh@ld").validate(false);
+	// expect("aiukjhldJ").validate(false);
+	// expect("5s@sdOddJ").validate(true);
+	// expect("}dddsldJ").validate(false);
+	// expect("5232!J").validate(false);
+	// expect("5sJ").validate(false);
